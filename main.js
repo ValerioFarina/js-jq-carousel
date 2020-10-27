232f6aaeb0e4323288a79b814538802f8a477a40
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-27 15:37:37 +0100

    Fatto in modo che la classe dell'immagine corrente venga cambiata da visible a hidden cliccando le frecce

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+
     // intercetto il click delle frecce
     $('.arrow').click(function() {
         // uso una variabile per ricordarmi qual è l'immagine corrente che viene mostrata
         var currentImg = $('img.visible');
-        console.log(currentImg);
+        // cambio la classe dell'immagine corrente da "visible" a "hidden"
+        currentImg.toggleClass('visible hidden');
     });
 
 // fine document.ready
