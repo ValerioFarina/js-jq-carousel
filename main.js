42f1dcc120d2718f74de8ddf7cb6fc00a72ebf0a
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-27 15:42:14 +0100

    Fatto in modo che cliccando la freccia di destra le immagini "scorrano" dalla prima all'utlima

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-    // intercetto il click delle frecce
-    $('.arrow').click(function() {
+    // intercetto il click della freccia di destra
+    $('#next .arrow').click(function() {
         // uso una variabile per ricordarmi qual è l'immagine corrente che viene mostrata
         var currentImg = $('img.visible');
         // cambio la classe dell'immagine corrente da "visible" a "hidden"
         currentImg.toggleClass('visible hidden');
+        // cambio la classe dell'immagine successiva a quella corrente da "hidden" a "visible"
+        currentImg.next().toggleClass('visible hidden');
     });
 
 // fine document.ready
