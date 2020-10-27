7c5ae88cfe28c0de24485c7054babc3e5290b0ac
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-27 15:32:41 +0100

    Creo una variabile per ricordarmi qual è l'immagine corrente che viene mostrata

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     // intercetto il click delle frecce
     $('.arrow').click(function() {
-        alert('Hai cliccato una freccia');
+        // uso una variabile per ricordarmi qual è l'immagine corrente che viene mostrata
+        var currentImg = $('img.visible');
+        console.log(currentImg);
     });
-    
+
 // fine document.ready
 });
