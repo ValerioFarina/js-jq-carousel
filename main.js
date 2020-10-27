1d3ffefc3535ebf86e6681d967cc1f293c651a78
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-27 15:50:20 +0100

    Creata funzione per controllare se un elemento ha un fratello immediatamente successivo

--- main.js
+++ main.js
@@ -6,9 +6,25 @@ $(document).ready(function() {
         var currentImg = $('img.visible');
         // cambio la classe dell'immagine corrente da "visible" a "hidden"
         currentImg.toggleClass('visible hidden');
+        console.log(hasNext(currentImg));
         // cambio la classe dell'immagine successiva a quella corrente da "hidden" a "visible"
         currentImg.next().toggleClass('visible hidden');
     });
 
 // fine document.ready
 });
+
+
+
+// ********** LE MIE FUNZIONI **********
+
+// questa funzione prende come parametro un elemento html selezionato facendo $(selector)
+// se l'elemento ha un fratello immediatamente successivo, allora ritorna true
+// altrimenti ritorna false
+function hasNext(element) {
+    if (element.next().length) {
+        return true;
+    } else {
+        return false;
+    }
+}
